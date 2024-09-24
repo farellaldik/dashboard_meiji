fd01cc3f0358a805ba4ada8a04b12ad5af0b012e
Farrel Aldi Kusuma <Farrel Aldi Kusuma@LP-MAP-01-FAK>
2024-09-24 15:32:34 +0700

    15:32 24/09/2024

--- Dashboard/Controllers/DashboardController.cs
+++ Dashboard/Controllers/DashboardController.cs
@@ -36,52 +36,33 @@ namespace Dashboard.Controllers
             if (!string.IsNullOrEmpty(employeeNik))
             {
                 var sql = @"
-                    WITH Results AS (
-                    SELECT s.SCHEDULE_ID, 
-                           n.ANAME, 
-                           s.EMPLOYEE_NIK, 
-                           CONVERT(DATE, n.CREATED_DATE) AS CREATED_DATE, 
-                           MAX(CONVERT(DATE, s.ANSWER_DATE)) AS ANSWER_DATE,
-                           SUM(s.CORRECT_COUNT) AS TOTAL_CORRECT_COUNT,
-                           SUM(s.IS_ANSWER) AS TOTAL_IS_ANSWER,
-                           COUNT(s.IS_ANSWER) AS QUESTIONS,
-                           CASE 
-                               WHEN COUNT(s.IS_ANSWER) = 0 THEN 0
-                               ELSE CAST((SUM(s.CORRECT_COUNT) * 100.0 / COUNT(s.IS_ANSWER)) AS DECIMAL (18,0))
-                           END AS SCORE,
-                           CASE 
-                               WHEN COUNT(s.IS_ANSWER) = 0 THEN 'TIDAK LULUS'
-                               ELSE CASE
-                                   WHEN (SUM(s.CORRECT_COUNT) * 100.0 / COUNT(s.IS_ANSWER)) >= 80 THEN 'LULUS'
-                                   ELSE 'TIDAK LULUS'
-                               END
-                           END AS KETERANGAN
-                    FROM TRAINING_TEST_SCHEDULE n
-                    JOIN TRAINING_EXAM_QUESTION s ON n.SCHEDULE_ID = s.SCHEDULE_ID
-                    GROUP BY s.SCHEDULE_ID, 
-                             n.ANAME, 
-                             s.EMPLOYEE_NIK, 
-                             CONVERT(DATE, n.CREATED_DATE)
-                ),
-                Grade_Count AS (
-                    SELECT v.MR_NIK,
-                           SUM(CASE WHEN v.DOCTOR_CLASS = 'A' AND v.VISIT = '1' THEN 1 ELSE 0 END) AS GRADE_A_COUNT,
-                           SUM(CASE WHEN v.DOCTOR_CLASS = 'B' AND v.VISIT = '1' THEN 1 ELSE 0 END) AS GRADE_B_COUNT,
-                           SUM(CASE WHEN v.DOCTOR_CLASS = 'C' AND v.VISIT = '1' THEN 1 ELSE 0 END) AS GRADE_C_COUNT
-                    FROM VISITING_JUKUDO_NOTES v
-                    WHERE v.VISIT = '1'
-                    GROUP BY v.MR_NIK
-                )
-                SELECT r.EMPLOYEE_NIK,
-                       SUM(CASE WHEN r.KETERANGAN = 'LULUS' THEN 1 ELSE 0 END) AS TOTAL_LULUS,
-                       SUM(CASE WHEN r.KETERANGAN = 'TIDAK LULUS' THEN 1 ELSE 0 END) AS TOTAL_TIDAK_LULUS,
-                       COALESCE(g.GRADE_A_COUNT, 0) AS GRADE_A_COUNT,
-                       COALESCE(g.GRADE_B_COUNT, 0) AS GRADE_B_COUNT,
-                       COALESCE(g.GRADE_C_COUNT, 0) AS GRADE_C_COUNT
-                FROM Results r
-                LEFT JOIN Grade_Count g ON r.EMPLOYEE_NIK = g.MR_NIK
-                WHERE r.EMPLOYEE_NIK = @EmployeeNik
-                GROUP BY r.EMPLOYEE_NIK, g.GRADE_A_COUNT, g.GRADE_B_COUNT, g.GRADE_C_COUNT;
+                        SELECT s.SCHEDULE_ID, 
+                               n.ANAME, 
+                               s.EMPLOYEE_NIK, 
+                               CONVERT(DATE, n.CREATED_DATE) AS CREATED_DATE, 
+                               MAX(CONVERT(DATE, s.ANSWER_DATE)) AS ANSWER_DATE,
+                               SUM(s.CORRECT_COUNT) AS TOTAL_CORRECT_COUNT,
+	                           COUNT(s.IS_ANSWER) - SUM(s.CORRECT_COUNT) AS TOTAL_FALSE_COUNT,
+                               SUM(s.IS_ANSWER) AS TOTAL_IS_ANSWER,
+                               COUNT(s.IS_ANSWER) AS QUESTIONS,
+                               CASE 
+                                   WHEN COUNT(s.IS_ANSWER) = 0 THEN 0
+                                   ELSE CAST((SUM(s.CORRECT_COUNT) * 100.0 / COUNT(s.IS_ANSWER)) AS DECIMAL(18,0))
+                               END AS SCORE,
+                               CASE 
+                                   WHEN COUNT(s.IS_ANSWER) = 0 THEN 'TIDAK LULUS'
+                                   ELSE CASE
+                                       WHEN (SUM(s.CORRECT_COUNT) * 100.0 / COUNT(s.IS_ANSWER)) >= 85 THEN 'Lulus'
+                                       ELSE 'Tidak Lulus'
+                                   END
+                               END AS KETERANGAN
+                        FROM TRAINING_TEST_SCHEDULE n
+                        JOIN TRAINING_EXAM_QUESTION s ON n.SCHEDULE_ID = s.SCHEDULE_ID
+                        WHERE s.EMPLOYEE_NIK = @EmployeeNik
+                        GROUP BY s.SCHEDULE_ID, 
+                                 n.ANAME, 
+                                 s.EMPLOYEE_NIK, 
+                                 CONVERT(DATE, n.CREATED_DATE)                
                 ";
 
                 var sql2 = @"
@@ -134,19 +115,33 @@ namespace Dashboard.Controllers
                     DATE_VISIT;
                 ";
 
+                var sql4 = @"
+                SELECT 
+                    MR_NIK,
+                    FORMAT(CONVERT(DATE, DATE_VISIT, 101), 'yyyy-MM') AS BULAN,
+                    COUNT(CASE WHEN DOCTOR_CLASS = 'A' THEN 1 END) AS TOTAL_VISIT_GRADE_A,
+                    COUNT(CASE WHEN DOCTOR_CLASS = 'B' THEN 1 END) AS TOTAL_VISIT_GRADE_B,
+                    COUNT(CASE WHEN DOCTOR_CLASS = 'C' THEN 1 END) AS TOTAL_VISIT_GRADE_C
+                FROM 
+                    VISITING_JUKUDO_NOTES_MOBILE 
+                WHERE 
+                    STATUS_VISIT = '1' 
+                    AND MR_NIK = @EmployeeNik
+                    AND DOCTOR_CLASS IN ('A', 'B', 'C') 
+                GROUP BY 
+                    MR_NIK, 
+                    FORMAT(CONVERT(DATE, DATE_VISIT, 101), 'yyyy-MM')
+                ORDER BY 
+                    BULAN;
+                ";
+
                 // Membuka koneksi dan mengeksekusi perintah
                 try
                 {
-                    var testResult = new
-                    {
-                        TotalLulus = 0,
-                        TotalTidakLulus = 0,
-                        TotalGradeA = 0,
-                        TotalGradeB = 0,
-                        TotalGradeC = 0
-                    };
+                    var tests = new List<dynamic>();
                     var visitToday = new List<dynamic>();
                     var visitLater = new List<dynamic>();
+                    var actualVisit = new List<dynamic>();
 
                     await using (var command = _context.Database.GetDbConnection().CreateCommand())
                     {
@@ -156,18 +151,23 @@ namespace Dashboard.Controllers
 
                         _context.Database.OpenConnection();
 
-                        await using (var result = await command.ExecuteReaderAsync())
+                        await using (var reader = await command.ExecuteReaderAsync())
                         {
-                            if (await result.ReadAsync())
+                            while (await reader.ReadAsync())
                             {
-                                testResult = new
+                                var testResult = new
                                 {
-                                    TotalLulus = result.GetInt32(result.GetOrdinal("TOTAL_LULUS")),
-                                    TotalTidakLulus = result.GetInt32(result.GetOrdinal("TOTAL_TIDAK_LULUS")),
-                                    TotalGradeA = result.GetInt32(result.GetOrdinal("GRADE_A_COUNT")),
-                                    TotalGradeB = result.GetInt32(result.GetOrdinal("GRADE_B_COUNT")),
-                                    TotalGradeC = result.GetInt32(result.GetOrdinal("GRADE_C_COUNT")),
+                                    Name = reader["ANAME"].ToString(),
+                                    CreatedDate = Convert.ToDateTime(reader["CREATED_DATE"]).ToString("yyyy-MM-dd"),
+                                    AnswerDate = reader["ANSWER_DATE"] != DBNull.Value ? Convert.ToDateTime(reader["ANSWER_DATE"]).ToString("yyyy-MM-dd") : null,
+                                    CorrectAnswers = Convert.ToInt32(reader["TOTAL_CORRECT_COUNT"]),
+                                    FalseAnswers = Convert.ToInt32(reader["TOTAL_FALSE_COUNT"]),
+                                    TotalQuestions = Convert.ToInt32(reader["QUESTIONS"]),
+                                    Score = Convert.ToInt32(reader["SCORE"]),
+                                    Remarks = reader["KETERANGAN"].ToString()
                                 };
+
+                                tests.Add(testResult);
                             }
                         }
 
@@ -219,13 +219,36 @@ namespace Dashboard.Controllers
                             }
                         }
 
+                        command.Parameters.Clear();
+                        command.CommandText = sql4;
+                        command.CommandType = System.Data.CommandType.Text;
+                        command.Parameters.Add(new SqlParameter("@EmployeeNik", employeeNik));
+
+                        await using (var reader = await command.ExecuteReaderAsync())
+                        {
+                            while (await reader.ReadAsync())
+                            {
+                                var actualvisit = new
+                                {
+                                    MrNik = Convert.ToInt32(reader["MR_NIK"]),
+                                    BulanTahun = Convert.ToDateTime(reader["BULAN"]).ToString("MMMM yyyy", new System.Globalization.CultureInfo("id-ID")),
+                                    TotalVisitA = Convert.ToInt32(reader["TOTAL_VISIT_GRADE_A"]),
+                                    TotalVisitB = Convert.ToInt32(reader["TOTAL_VISIT_GRADE_B"]),
+                                    TotalVisitC = Convert.ToInt32(reader["TOTAL_VISIT_GRADE_C"]),
+                                };
+
+                                actualVisit.Add(actualvisit);
+                            }
+                        }
+
                         return Json
                             (
                                 new 
                                 { 
-                                    TestResults = testResult,
+                                    TestResults = tests,
                                     VisitToday = visitToday,
                                     VisitLater = visitLater,
+                                    ActualVisit = actualVisit,
                                 }
                             );
                     }
